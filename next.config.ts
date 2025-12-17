40626d5dd85242d8694be72b37fa0895c54b30fd
sarath <sarathsb2003@gmail.com>
2025-12-18 01:34:59 +0530

    output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',    // Build for Node.js server
   images: { unoptimized: true },
   reactStrictMode: true,
 };
